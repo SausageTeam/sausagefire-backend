d759f553e406ca5c391bc3deb3060cfee038c461
Martin Li <lixphilosophy@gmail.com>
2020-03-17 20:12:25 -0400

    add structure

---
+++ src/main/java/com/sausage/app/controller/hr/HouseManageController.java
@@ -0,0 +1,9 @@
+package com.sausage.app.controller.hr;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hr/housing")
+public class HouseManageController {
+}
