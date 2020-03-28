7e735a1ee08057cd1051685f523e0857499266d5
WentaoHu <wentaohu1017@gmail.com>
2020-03-27 20:32:21 -0400

    comment out filter

--- src/main/java/com/sausage/app/config/FilterConfig.java
+++ src/main/java/com/sausage/app/config/FilterConfig.java
@@ -1,20 +1,20 @@
-package com.sausage.app.config;
-
-
-import com.sausage.app.security.filter.AuthFilter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Collections;
-
-@Configuration
-public class FilterConfig {
-
-    @Value("${services.auth}")
-    private String authService;
-
+//package com.sausage.app.config;
+//
+//
+//import com.sausage.app.security.filter.AuthFilter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.Collections;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    @Value("${services.auth}")
+//    private String authService;
+//
 //    @Bean
 //    public FilterRegistrationBean<AuthFilter> authFilterConfig() {
 //        final FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
@@ -23,5 +23,5 @@ public class FilterConfig {
 //        registrationBean.addUrlPatterns("/*");
 //        return registrationBean;
 //    }
-
-}
+//
+//}
--- src/main/java/com/sausage/app/security/filter/AuthFilter.java
+++ src/main/java/com/sausage/app/security/filter/AuthFilter.java
@@ -1,33 +1,33 @@
-package com.sausage.app.security.filter;
-
-import com.sausage.app.constant.Constant;
-import com.sausage.app.security.util.JwtUtil;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-public class AuthFilter extends OncePerRequestFilter {
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        String userId = JwtUtil.getSubject(httpServletRequest, Constant.JWT_TOKEN_COOKIE_NAME, Constant.SIGNING_KEY);
-        System.out.println("USER ID: " + userId);
-        if (userId == null) {
-            System.out.println("NULL, REDIRECT");
-            String authService = this.getFilterConfig().getInitParameter("services.auth");
-            System.out.println("FROM: " + authService);
-            System.out.println("REDIRECT: " + httpServletRequest.getRequestURL());
-            httpServletResponse.sendRedirect(authService + "?redirect=" + httpServletRequest.getRequestURL());
-        } else {
-            System.out.println("NOT NULL");
-            filterChain.doFilter(httpServletRequest, httpServletResponse);
-        }
-    }
-
-}
+//package com.sausage.app.security.filter;
+//
+//import com.sausage.app.constant.Constant;
+//import com.sausage.app.security.util.JwtUtil;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Component
+//public class AuthFilter extends OncePerRequestFilter {
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+//        String userId = JwtUtil.getSubject(httpServletRequest, Constant.JWT_TOKEN_COOKIE_NAME, Constant.SIGNING_KEY);
+//        System.out.println("USER ID: " + userId);
+//        if (userId == null) {
+//            System.out.println("NULL, REDIRECT");
+//            String authService = this.getFilterConfig().getInitParameter("services.auth");
+//            System.out.println("FROM: " + authService);
+//            System.out.println("REDIRECT: " + httpServletRequest.getRequestURL());
+//            httpServletResponse.sendRedirect(authService + "?redirect=" + httpServletRequest.getRequestURL());
+//        } else {
+//            System.out.println("NOT NULL");
+//            filterChain.doFilter(httpServletRequest, httpServletResponse);
+//        }
+//    }
+//
+//}
