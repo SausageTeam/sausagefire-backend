1625fb69e0d98389d562ab600683ff8d5bc0a57c
WentaoHu <wentaohu1017@gmail.com>
2020-03-28 03:41:11 -0400

    employee profile

--- src/main/java/com/sausage/app/controller/hr/HREmployeeProfileController.java
+++ src/main/java/com/sausage/app/controller/hr/HREmployeeProfileController.java
@@ -3,12 +3,16 @@ package com.sausage.app.controller.hr;
 import com.sausage.app.domain.common.GenericResponse;
 import com.sausage.app.domain.common.ServiceStatus;
 import com.sausage.app.domain.hr.employeeProfile.*;
+import com.sausage.app.security.util.JwtUtil;
 import com.sausage.app.service.hr.employeeProfile.HREmployeeProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.sausage.app.constant.Constant.JWT_TOKEN_COOKIE_NAME;
+import static com.sausage.app.constant.Constant.SIGNING_KEY;
 
 @RestController
 @RequestMapping("/hr/employee-profile")
@@ -23,28 +27,38 @@ public class HREmployeeProfileController {
 
     @GetMapping
     public @ResponseBody
-    EmployeeProfileGetResponse getEmployeeProfile(HttpServletRequest httpServletRequest){
+    EmployeeProfileGetResponse getEmployeeProfile(HttpServletRequest httpServletRequest) {
         EmployeeProfileGetResponse employeeProfileGetResponse = new EmployeeProfileGetResponse();
-        //        int userId = Integer.parseInt(JwtUtil.getSubject(httpServletRequest, Constant.JWT_TOKEN_COOKIE_NAME, Constant.SIGNING_KEY));
-        int userId = 2;
-        EmployeeProfile employeeProfile = HREmployeeProfileService.getEmployeeProfile();
-        employeeProfileGetResponse.setEmployeeProfile(employeeProfile);
-        prepareResponse(employeeProfileGetResponse, true, "");
+        String id = JwtUtil.getSubject(httpServletRequest, JWT_TOKEN_COOKIE_NAME, SIGNING_KEY);
+        if (id == null) {
+            prepareResponse(employeeProfileGetResponse, "401", false, "User not Found");
+        } else {
+            EmployeeProfile employeeProfile = HREmployeeProfileService.getEmployeeProfile();
+            if (employeeProfile == null) {
+                prepareResponse(employeeProfileGetResponse, "500", false, "Unexpected Error");
+            } else {
+                employeeProfileGetResponse.setEmployeeProfile(employeeProfile);
+                prepareResponse(employeeProfileGetResponse, "200", true, "");
+            }
+        }
         return employeeProfileGetResponse;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
-    EmployeeProfilePostResponse postEmployeeProfile(@RequestBody EmployeeProfilePostRequest employeeProfilePostRequest){
+    EmployeeProfilePostResponse postEmployeeProfile(HttpServletRequest httpServletRequest, @RequestBody EmployeeProfilePostRequest employeeProfilePostRequest) {
         EmployeeProfilePostResponse employeeProfilePostResponse = new EmployeeProfilePostResponse();
-        //        int userId = Integer.parseInt(JwtUtil.getSubject(httpServletRequest, Constant.JWT_TOKEN_COOKIE_NAME, Constant.SIGNING_KEY));
-        int userId = 2;
-
-        prepareResponse(employeeProfilePostResponse, true, "");
+        String id = JwtUtil.getSubject(httpServletRequest, JWT_TOKEN_COOKIE_NAME, SIGNING_KEY);
+        if (id == null) {
+            prepareResponse(employeeProfilePostResponse, "401", false, "User not Found");
+        } else {
+            prepareResponse(employeeProfilePostResponse, "200", true, "");
+        }
         return employeeProfilePostResponse;
     }
 
-    private void prepareResponse(GenericResponse response, boolean success, String errorMessage) {
-        response.setServiceStatus(new ServiceStatus(success ? "SUCCESS" : "FAILED", success, errorMessage));
+    private void prepareResponse(GenericResponse response, String statusCode, boolean success, String errorMessage) {
+        response.setServiceStatus(new ServiceStatus(statusCode, success, errorMessage));
     }
+
 }
