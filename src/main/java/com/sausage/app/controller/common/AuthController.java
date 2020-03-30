647af132eff1e840284b7b57f6d98081d8272a70
WentaoHu <wentaohu1017@gmail.com>
2020-03-29 21:56:35 -0400

    auth add header

--- src/main/java/com/sausage/app/controller/common/AuthController.java
+++ src/main/java/com/sausage/app/controller/common/AuthController.java
@@ -7,6 +7,10 @@ import com.sausage.app.domain.common.auth.AuthGetResponse;
 import com.sausage.app.security.util.JwtUtil;
 import com.sausage.app.service.common.auth.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,25 +32,35 @@ public class AuthController {
     }
 
     @GetMapping("/auth")
-    public @ResponseBody
-    AuthGetResponse getAuth(HttpServletRequest httpServletRequest) {
+    public ResponseEntity<String> getAuth(HttpServletRequest httpServletRequest) {
+        ResponseEntity<String> responseEntity;
+        HttpHeaders httpHeaders = new HttpHeaders();
+
         AuthGetResponse authGetResponse = new AuthGetResponse();
         String id = JwtUtil.getSubject(httpServletRequest, JWT_TOKEN_COOKIE_NAME, SIGNING_KEY);
         if (id == null) {
-            authGetResponse.setRedirectUrl(AUTH_SERVICE);
-            prepareResponse(authGetResponse, "401", false, "User not Found");
+            httpHeaders.add("redirectUrl", AUTH_SERVICE);
+            responseEntity = ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .headers(httpHeaders)
+                    .body("Sorry, you need to login first 😅");
         } else {
             int userId = Integer.parseInt(id);
             Auth auth = authService.getAuth(userId);
             if (auth == null) {
-                authGetResponse.setRedirectUrl(AUTH_SERVICE);
-                prepareResponse(authGetResponse, "500", false, "Unexpected Error");
+                httpHeaders.add("redirectUrl", AUTH_SERVICE);
+                responseEntity = ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .headers(httpHeaders)
+                        .body("Sorry, no data found 😅");
             } else {
-                authGetResponse.setAuth(auth);
-                prepareResponse(authGetResponse, "200", true, "");
+                httpHeaders.add("roleId", String.valueOf(auth.getRoleId()));
+                httpHeaders.add("onboardingStatus", String.valueOf(auth.getOnboardingStatus()));
+                httpHeaders.add("ifNeedVisa", String.valueOf(auth.isIfNeedVisa()));
+                responseEntity = ResponseEntity.ok()
+                        .headers(httpHeaders)
+                        .body("Hello 😇");
             }
         }
-        return authGetResponse;
+        return responseEntity;
     }
 
     private void prepareResponse(GenericResponse response, String statusCode, boolean success, String errorMessage) {
