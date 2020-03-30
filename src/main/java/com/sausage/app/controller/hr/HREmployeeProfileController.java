f100563c5c7abad7113adb7abaefa03c526a6709
WentaoHu <wentaohu1017@gmail.com>
2020-03-30 00:22:04 -0400

    employee profile add responseEntity

--- src/main/java/com/sausage/app/controller/hr/HREmployeeProfileController.java
+++ src/main/java/com/sausage/app/controller/hr/HREmployeeProfileController.java
@@ -1,12 +1,16 @@
 package com.sausage.app.controller.hr;
 
-import com.sausage.app.domain.common.GenericResponse;
-import com.sausage.app.domain.common.ServiceStatus;
-import com.sausage.app.domain.hr.employeeProfile.*;
+import com.sausage.app.domain.hr.employeeProfile.EmployeeProfile;
+import com.sausage.app.domain.hr.employeeProfile.EmployeeProfileGetResponse;
+import com.sausage.app.domain.hr.employeeProfile.EmployeeProfilePostRequest;
+import com.sausage.app.domain.hr.employeeProfile.EmployeeProfilePostResponse;
 import com.sausage.app.security.util.JwtUtil;
 import com.sausage.app.service.hr.employeeProfile.HREmployeeProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,39 +30,49 @@ public class HREmployeeProfileController {
     }
 
     @GetMapping
-    public @ResponseBody
-    EmployeeProfileGetResponse getEmployeeProfile(HttpServletRequest httpServletRequest) {
+    public ResponseEntity<Object> getEmployeeProfile(HttpServletRequest httpServletRequest) {
+        ResponseEntity<Object> responseEntity;
+        HttpHeaders httpHeaders = new HttpHeaders();
+
         EmployeeProfileGetResponse employeeProfileGetResponse = new EmployeeProfileGetResponse();
         String id = JwtUtil.getSubject(httpServletRequest, JWT_TOKEN_COOKIE_NAME, SIGNING_KEY);
         if (id == null) {
-            prepareResponse(employeeProfileGetResponse, "401", false, "User not Found");
+            responseEntity = ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .headers(httpHeaders)
+                    .body("Sorry, you are not authorized 😅");
         } else {
             EmployeeProfile employeeProfile = HREmployeeProfileService.getEmployeeProfile();
             if (employeeProfile == null) {
-                prepareResponse(employeeProfileGetResponse, "500", false, "Unexpected Error");
+                responseEntity = ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .headers(httpHeaders)
+                        .body("Sorry, no data found 😅");
             } else {
                 employeeProfileGetResponse.setEmployeeProfile(employeeProfile);
-                prepareResponse(employeeProfileGetResponse, "200", true, "");
+                responseEntity = ResponseEntity.ok()
+                        .headers(httpHeaders)
+                        .body(employeeProfileGetResponse);
             }
         }
-        return employeeProfileGetResponse;
+        return responseEntity;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    EmployeeProfilePostResponse postEmployeeProfile(HttpServletRequest httpServletRequest, @RequestBody EmployeeProfilePostRequest employeeProfilePostRequest) {
+    public ResponseEntity<Object> postEmployeeProfile(HttpServletRequest httpServletRequest, @RequestBody EmployeeProfilePostRequest employeeProfilePostRequest) {
+        ResponseEntity<Object> responseEntity;
+        HttpHeaders httpHeaders = new HttpHeaders();
+
         EmployeeProfilePostResponse employeeProfilePostResponse = new EmployeeProfilePostResponse();
         String id = JwtUtil.getSubject(httpServletRequest, JWT_TOKEN_COOKIE_NAME, SIGNING_KEY);
         if (id == null) {
-            prepareResponse(employeeProfilePostResponse, "401", false, "User not Found");
+            responseEntity = ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .headers(httpHeaders)
+                    .body("Sorry, you are not authorized 😅");
         } else {
-            prepareResponse(employeeProfilePostResponse, "200", true, "");
+            responseEntity = ResponseEntity.ok()
+                    .headers(httpHeaders)
+                    .body(employeeProfilePostResponse);
         }
-        return employeeProfilePostResponse;
-    }
-
-    private void prepareResponse(GenericResponse response, String statusCode, boolean success, String errorMessage) {
-        response.setServiceStatus(new ServiceStatus(statusCode, success, errorMessage));
+        return responseEntity;
     }
 
 }
